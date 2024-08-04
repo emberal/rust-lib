347314460af0e0c286380ee7099a3ebcd844368c
Martin Berg Alstad <emberal+github@pm.me>
2024-08-05 01:49:28 +0200

    Sanitize impl in macro

--- crates/into_response_derive/src/derive.rs
+++ crates/into_response_derive/src/derive.rs
@@ -7,7 +7,7 @@ pub fn into_response_derive_impl(input: DeriveInput) -> TokenStream {
     let name = &input.ident;
 
     let expanded = quote! {
-        impl IntoResponse for #name {
+        impl axum::response::IntoResponse for #name {
             fn into_response(self) -> axum::response::Response {
                 let version = env!("CARGO_PKG_VERSION");
                 lib::serde::response::BaseResponse::new(version, self)
