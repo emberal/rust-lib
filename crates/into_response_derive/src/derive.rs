ee487540ac217c723c721fd5c2507ff81148c2e3
Martin Berg Alstad <emberal+github@pm.me>
2024-08-05 01:46:35 +0200

    Sanitize returntype in macro

--- crates/into_response_derive/src/derive.rs
+++ crates/into_response_derive/src/derive.rs
@@ -8,7 +8,7 @@ pub fn into_response_derive_impl(input: DeriveInput) -> TokenStream {
 
     let expanded = quote! {
         impl IntoResponse for #name {
-            fn into_response(self) -> Response {
+            fn into_response(self) -> axum::response::Response {
                 let version = env!("CARGO_PKG_VERSION");
                 lib::serde::response::BaseResponse::new(version, self)
                     .into_response()
