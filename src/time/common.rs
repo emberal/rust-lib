3318aacf7ced1bb808ed7ad0a7b79d090aa78479
Martin Berg Alstad <emberal+github@pm.me>
2024-08-24 19:29:19 +0200

    Added more derives to DateTimeInterval

--- src/time/common.rs
+++ src/time/common.rs
@@ -1,7 +1,8 @@
 use chrono::NaiveDateTime;
 use derive_more::Constructor;
 
-#[derive(Debug, Clone, Copy, Constructor)]
+#[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Constructor)]
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct DateTimeInterval {
     pub start: NaiveDateTime,
     pub end: NaiveDateTime,
